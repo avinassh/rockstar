eea72983df3301d32553a7a2755633ed07b050a8
Duane F. King <duane.king@duanefking.com>
2015-07-19 06:57:56 +0000

    Added Dart example.

---
+++ examples/dart_rockstar.py
@@ -0,0 +1,8 @@
+from RockStar import RockStar
+
+dart_code = """void main() {
+  print('Hello World!');
+}"""
+
+rock_it_bro = RockStar(days=400, file_name='helloworld.dart', code=dart_code)
+rock_it_bro.make_me_a_rockstar()
