b3ecb5445f9d5f01de3beddb0a8d60da6375c3a8
Duane F. King <duane.king@duanefking.com>
2015-07-19 07:22:06 +0000

    Remove dart example

--- examples/dart_rockstar.py
+++
@@ -1,8 +0,0 @@
-from RockStar import RockStar
-
-dart_code = """void main() {
-  print('Hello World!');
-}"""
-
-rock_it_bro = RockStar(days=400, file_name='helloworld.dart', code=dart_code)
-rock_it_bro.make_me_a_rockstar()
