7c45e3033f672c9f5ebafd97c59a3b45355b5d95
Brian Teller <brianateller@gmail.com>
2015-07-19 01:52:41 -0400

    Created a DART version

---
+++ examples/dart_rockstar.py
@@ -0,0 +1,5 @@
+from RockStar import RockStar
+
+dart_code = "main() { print('Hello Word'); }"
+rock_it_bro = RockStar(days=400, file_name='helloWorld.dart', code=dart_code)
+rock_it_bro.make_me_a_rockstar()
