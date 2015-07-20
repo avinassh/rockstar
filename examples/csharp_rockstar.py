30bd80152af0dcee75d1f338fcdc70ef67b3ca0e
rahulxxarora@gmail.com <rahulxxarora@gmail.com>
2015-07-20 19:23:54 +0530

    Added csharp code

---
+++ examples/csharp_rockstar.py
@@ -0,0 +1,5 @@
+from RockStar import RockStar
+
+csharp_code = "System.Console.WriteLine('Hello, World!');"
+rock_it_bro = RockStar(days=400, file_name='helloWorld.cs', code=csharp_code)
+rock_it_bro.make_me_a_rockstar()
