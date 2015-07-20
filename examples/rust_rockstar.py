93c098ae736350c4dc52f19b29978ef4fbc54abb
George Marshall <george@georgemarshall.name>
2015-07-19 17:50:46 -0700

    Added rust example

---
+++ examples/rust_rockstar.py
@@ -0,0 +1,5 @@
+from RockStar import RockStar
+
+rust_code = 'fn main() { println!("Hello world") }'
+rock_it_bro = RockStar(days=400, file_name='helloWorld.rs', code=rust_code)
+rock_it_bro.make_me_a_rockstar()
