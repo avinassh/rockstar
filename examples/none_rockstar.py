08985f78ab4feccdb9bc1931927978f5092dcbfb
avi <hi@avi.im>
2015-07-20 11:21:24 +0530

    added none and r examples

---
+++ examples/none_rockstar.py
@@ -0,0 +1,12 @@
+from RockStar import RockStar
+
+# yup, there is a programming language called `none`
+# https://bitbucket.org/duangle/none
+# you better be a rockstar in none.
+
+none_code = """(none
+(print "Hello World")
+)"""
+
+rock_it_bro = RockStar(days=400, file_name='hello.n', code=none_code)
+rock_it_bro.make_me_a_rockstar()
