fdcb0bc502ea3976f7edd613f0bdb0857104fc82
Gökay Gürcan <gokaygurcan@gmail.com>
2015-07-31 01:16:40 +0300

    Fix AGS (Adventure Game Studio) example

--- examples/ags_rockstar.py
+++ examples/ags_rockstar.py
@@ -1,5 +1,5 @@
 from rockstar import RockStar
 
-ags_code = "Display("Hello, world!");"
+ags_code = 'Display("Hello, world!");'
 rock_it_bro = RockStar(days=777, file_name='helloworld.asc', code=ags_code)
 rock_it_bro.make_me_a_rockstar()
