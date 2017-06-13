549052279c9d31ae1e0057d68b9730782a577658
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-06-12 23:39:01 -0300

    jupyter_rockstar.py

---
+++ examples/jupyter_rockstar.py
@@ -0,0 +1,8 @@
+from rockstar import RockStar
+
+jupyter_code = """'text': [
+  "Hello, world!\n"
+]"""
+
+rock_it_bro = RockStar(days=400, file_name='helloworld.ipynb', code=jupyter_code)
+rock_it_bro.make_me_a_rockstar()
