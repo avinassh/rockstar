ac232b8b78fe2e5ada5685f547854299473e05e4
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-06-12 23:49:27 -0300

    updating

--- examples/jupyter_rockstar.py
+++ examples/jupyter_rockstar.py
@@ -1,8 +1,10 @@
 from rockstar import RockStar
 
-jupyter_code = """'text': [
-  "Hello, world!\n"
-]"""
+jupyter_code = """
+'text': [
+  'Hello, world!'
+]
+"""
 
-rock_it_bro = RockStar(days=400, file_name='helloworld.ipynb', code=jupyter_code)
+rock_it_bro = RockStar(days=100, file_name='helloworld.ipynb', code=jupyter_code)
 rock_it_bro.make_me_a_rockstar()
