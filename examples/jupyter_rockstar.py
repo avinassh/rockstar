49b5c14dd90c4e548c84789bec0ce2aa1a62d549
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-06-13 00:00:27 -0300

    new example

--- examples/jupyter_rockstar.py
+++ examples/jupyter_rockstar.py
@@ -1,10 +1,16 @@
 from rockstar import RockStar
 
 jupyter_code = """
-'text': [
-  'Hello, world!'
-]
+{
+	"cells": [{
+		"cell_type": "markdown",
+		"metadata": {},
+		"source": [
+			"# Hello, world!"
+		]
+	}]
+}
 """
 
-rock_it_bro = RockStar(days=400, file_name='helloworld.ipynb', code=jupyter_code)
+rock_it_bro = RockStar(days=100, file_name='helloworld.ipynb', code=jupyter_code)
 rock_it_bro.make_me_a_rockstar()
