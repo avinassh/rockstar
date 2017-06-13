cc96aed2f39e56f770a7e01b729392003a5c3d47
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-06-12 23:50:46 -0300

    400 days

--- examples/jupyter_rockstar.py
+++ examples/jupyter_rockstar.py
@@ -6,5 +6,5 @@ jupyter_code = """
 ]
 """
 
-rock_it_bro = RockStar(days=100, file_name='helloworld.ipynb', code=jupyter_code)
+rock_it_bro = RockStar(days=400, file_name='helloworld.ipynb', code=jupyter_code)
 rock_it_bro.make_me_a_rockstar()
