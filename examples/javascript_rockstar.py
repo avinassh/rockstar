da2cc59e48d4577088614fa22bbc3d6100a870f3
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-11-21 12:18:53 -0200

    println => console.log

--- examples/javascript_rockstar.py
+++ examples/javascript_rockstar.py
@@ -1,5 +1,5 @@
 from rockstar import RockStar
 
-js_code = "println('Hello world')"
+js_code = "console.log('Hello world')"
 rock_it_bro = RockStar(days=400, file_name='helloWorld.js', code=js_code)
 rock_it_bro.make_me_a_rockstar()
